72e3dbb7cb756d53d5604b12e37164e3cda05b0f
Rafik <rafik.remal@r-bim.fr>
2023-04-13 19:06:05 +0200

    add try statement to the creation of the tab in case the tab already existFinal update

--- App.cs
+++ App.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Controls;
+using System.Diagnostics;
 
 #endregion
 
@@ -27,12 +28,20 @@ namespace RAB_Session06Challenge
 
 
             //Create a ribbon tab called "Revit Add-in Bootcamp"
+            try
+            {
+                a.CreateRibbonTab("Revit Add-in Bootcamp");
+            }
+            catch (Exception)
+            {
+                Debug.Print("tab already exists.");
 
-            a.CreateRibbonTab("Revit Add-in Bootcamp");
+                throw;
+            }
+
 
 
             //Create a ribbon panel called "Revit Tools"
-
            RibbonPanel ribbonPanel1 = a.CreateRibbonPanel("Revit Add-in Bootcamp", "Revit Tools");
 
             //Create push buttons (Tool 1, Tool 2)
@@ -48,7 +57,6 @@ namespace RAB_Session06Challenge
             PushButtonData pushButtonData10 = new PushButtonData("ToolN10", "Tool10", assemblyName, "RAB_Session06Challenge.CmdCommand10");
            
             //Add images
-
             pushButtonData1.LargeImage = new BitmapImage(new Uri(path + @"\Blue_32.png"));
             pushButtonData2.LargeImage = new BitmapImage(new Uri(path + @"\Green_32.png"));
             pushButtonData3.Image = new BitmapImage(new Uri(path + @"\Red_16.png"));
@@ -61,19 +69,17 @@ namespace RAB_Session06Challenge
             pushButtonData10.LargeImage = new BitmapImage(new Uri(path + @"\Red_32.png"));
 
             //add tooltips
-            pushButtonData1.ToolTip = "this is a tool tip for push button1";
-            pushButtonData2.ToolTip = "this is a tool tip for push button2";
-            pushButtonData3.ToolTip = "this is a tool tip for push button3";
-            pushButtonData4.ToolTip = "this is a tool tip for push button4";
-            pushButtonData5.ToolTip = "this is a tool tip for push button5";
-            pushButtonData6.ToolTip = "this is a tool tip for push button6";
-            pushButtonData7.ToolTip = "this is a tool tip for push button7";
-            pushButtonData8.ToolTip = "this is a tool tip for push button8";
-            pushButtonData9.ToolTip = "this is a tool tip for push button9";
-            pushButtonData10.ToolTip = "this is a tool tip for push button10";
-
-
-
+            pushButtonData1.ToolTip = "This is a tool tip for push button1";
+            pushButtonData2.ToolTip = "This is a tool tip for push button2";
+            pushButtonData3.ToolTip = "This is a tool tip for push button3";
+            pushButtonData4.ToolTip = "This is a tool tip for push button4";
+            pushButtonData5.ToolTip = "This is a tool tip for push button5";
+            pushButtonData6.ToolTip = "This is a tool tip for push button6";
+            pushButtonData7.ToolTip = "This is a tool tip for push button7";
+            pushButtonData8.ToolTip = "This is a tool tip for push button8";
+            pushButtonData9.ToolTip = "This is a tool tip for push button9";
+            pushButtonData10.ToolTip = "This is a tool tip for push button10";
+           
 
             //Add buttons to the ribbon panel
             ribbonPanel1.AddItem(pushButtonData1);
@@ -83,7 +89,6 @@ namespace RAB_Session06Challenge
             ribbonPanel1.AddSeparator();
 
             //Create 1 stacked row containing 3 push buttons (Tool 3, Tool 4, Tool 5)
-
             ribbonPanel1.AddStackedItems(pushButtonData3, pushButtonData4,pushButtonData5);
 
             //Add a separator
@@ -95,16 +100,17 @@ namespace RAB_Session06Challenge
             sb1.AddPushButton(pushButtonData6);
             sb1.AddPushButton(pushButtonData7);
 
-            //Create 1 pulldown button (called "More Tools) containing 3 push buttons (Tool 8, Tool 9, Tool 10)
+            //Add a separator
+            ribbonPanel1.AddSeparator();
 
+            //Create 1 pulldown button (called "More Tools) containing 3 push buttons (Tool 8, Tool 9, Tool 10)
             PulldownButtonData pulldownButtonData = new PulldownButtonData("More Tools", "PullDown Button Data");
             PulldownButton pdb1 = ribbonPanel1.AddItem(pulldownButtonData) as PulldownButton;
             pdb1.LargeImage = new BitmapImage(new Uri(path + @"\Yellow_32.png"));
             pdb1.AddPushButton(pushButtonData8);
             pdb1.AddPushButton(pushButtonData9);
             pdb1.AddPushButton(pushButtonData10);
-
-
+            pdb1.ToolTip = "This is a tool tip for pulldownButtonData";
 
 
             return Result.Succeeded;
